77251bff1707e9977ebfd58bebe04c7040a1a7f5
thtuan1995@gmail <thtuan1995@gmail.com>
2023-05-16 22:24:23 +0700

    Update

--- lib/sources/ext.dart
+++ lib/sources/ext.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 
 extension Dimension on num {
   EdgeInsets get top => EdgeInsets.only(top: toDouble());
+
   EdgeInsets get left => EdgeInsets.only(left: toDouble());
+
   EdgeInsets get right => EdgeInsets.only(right: toDouble());
+
   EdgeInsets get bottom => EdgeInsets.only(bottom: toDouble());
+
   EdgeInsets get vertical => EdgeInsets.symmetric(vertical: toDouble());
+
   EdgeInsets get horizontal => EdgeInsets.symmetric(horizontal: toDouble());
+
   EdgeInsets get all => EdgeInsets.all(toDouble());
 
   SizedBox get width => SizedBox(width: toDouble());
-  SizedBox get height => SizedBox(height: toDouble());
 
+  SizedBox get height => SizedBox(height: toDouble());
 }
 
 extension ColorParserString on String {
@@ -30,3 +37,12 @@ extension ColorParserInt on int {
   }
 }
 
+extension TimeParser on Duration {
+  String get display {
+    final hour = (inSeconds / 3600).floor();
+    final min = ((inSeconds / 60) % 60).floor();
+    final sec = inSeconds % 60;
+    final formatter = NumberFormat('00');
+    return '${formatter.format(hour)}:${formatter.format(min)}:${formatter.format(sec)}';
+  }
+}
